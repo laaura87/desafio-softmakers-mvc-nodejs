657c4f58732194160897a0de0d9b21f7f5e0a135
Laura Izabel <lauraizabel2563@gmail.com>
2020-09-13 18:12:56 -0300

    file filter

--- src/config/multer.js
+++ src/config/multer.js
@@ -13,4 +13,14 @@ module.exports = {
       callback(null, fileName);
     },
   }),
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+      return callback(new Error("Only images are allowed"));
+    }
+    callback(null, true);
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 };
